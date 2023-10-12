dda6b748ff73be93e64a98a0c944b3f27d76d6d8
shimjuwan <rd002@naver.com>
2023-10-12 14:14:48 +0900

    version6

--- app/src/main/java/com/example/cherry/message/MyMsgActivity.kt
+++ app/src/main/java/com/example/cherry/message/MyMsgActivity.kt
@@ -1,7 +1,12 @@
 package com.example.cherry.message
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ListView
 import com.example.cherry.R
 import com.example.cherry.auth.UserDataModel
@@ -10,10 +15,15 @@ import com.example.cherry.utils.FirebaseUtils
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.example.cherry.utils.MyInfo
+import com.example.cherry.message.MyLikeListActivity
 
 class MyMsgActivity : AppCompatActivity() {
     lateinit var listViewAdapter : MsgAdapter
     val msgList = mutableListOf<MsgModel>()
+    lateinit var getterUid : String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_msg)
@@ -26,6 +36,12 @@ class MyMsgActivity : AppCompatActivity() {
 
         //get msg
         getMyMsg()
+
+        //search
+        val searchBtn = findViewById<Button>(R.id.search_button)
+        searchBtn.setOnClickListener{
+            searchUser()
+        }
     }
 
     //get my msg from firebase
@@ -48,5 +64,57 @@ class MyMsgActivity : AppCompatActivity() {
             }
         }
         FirebaseRef.userMsgRef.child(FirebaseUtils.getUid()).addValueEventListener(postListener)
+    }
+
+    //search user
+    private fun searchUser() {
+        //input
+        val searchEditText = findViewById<EditText>(R.id.search_EditText)
+
+        val msgBuilder = AlertDialog.Builder(this)
+            .setPositiveButton("확인") { dialogInterface, i -> }
+        val targetName : String = searchEditText.text.toString()
+
+        val index : Int? = msgList.binarySearch { String.CASE_INSENSITIVE_ORDER.compare(it.senderInfo, targetName) }
+
+        //cant find
+        if(index == -1) {
+            msgBuilder.setTitle("검색 오류")
+            msgBuilder.setMessage("이름을 찾을 수 없습니다")
+        }
+        //find
+        else {
+            msgBuilder.setTitle("검색 성공")
+            msgBuilder.setMessage(targetName + "님을 찾았습니다")
+            msgBuilder.setPositiveButton("메시지 보내기", DialogInterface.OnClickListener(){ dialogInterface, i ->
+                showDialog()
+            })
+        }
+
+        //dialog
+        val msgDlg: AlertDialog = msgBuilder.create()
+        msgDlg.show()
+    }
+
+    //Dialog
+    private fun showDialog(){
+        val mDialogView = LayoutInflater.from(this).inflate(R.layout.custom_dialog,null)
+        val mBuilder = AlertDialog.Builder(this)
+            .setView(mDialogView)
+            .setTitle("메시지 보내기")
+
+        val mAlertDialog = mBuilder.show()
+
+        val btn = mAlertDialog.findViewById<Button>(R.id.sendBtnArea)
+        val textArea = mAlertDialog.findViewById<EditText>(R.id.sendTextArea)
+        btn?.setOnClickListener {
+            val mgsModel = MsgModel(MyInfo.myNickname,textArea!!.text.toString())
+
+            //send msginfo to firebase
+            FirebaseRef.userMsgRef.child(getterUid).push().setValue(mgsModel)
+
+            //off dialog
+            mAlertDialog.dismiss()
+        }
     }
 }
